b291c6426e303fdef534fa36ef36a4538c30bd0f
Robert Petersson <rpetersson@gmail.com>
2024-12-15 15:25:35 +0100

    update

--- script.ps1
+++ script.ps1
@@ -2,10 +2,12 @@ param(
     $outputpath = ".\"
 )
 
+# Connect and set the subscription context
+Connect-AzAccount 
+
 $subscriptionIds = Get-AzSubscription | Select-Object -ExpandProperty Id
 
-# Connect and set the subscription context
-Connect-AzAccount -SubscriptionId $subscriptionId
+
 # Initialize an empty array to store results
 $resultsArray = @()
 
@@ -13,140 +15,115 @@ $resultsArray = @()
 
 foreach ($subscriptionId in $subscriptionIds) {
 
-# Set the subscription context
-Set-AzContext -SubscriptionId $subscriptionId
+    # Set the subscription context
+    Set-AzContext -SubscriptionId $subscriptionId
 
-# Define time range
-$timeFrame = (Get-Date).AddDays(-30)
-$startDate = $timeFrame.ToUniversalTime().ToString("yyyy-MM-ddTHH:mm:ssZ")
-$endDate = (Get-Date).ToUniversalTime().ToString("yyyy-MM-ddTHH:mm:ssZ")
+    # Define time range
+    $timeFrame = (Get-Date).AddDays(-30)
+    $startDate = $timeFrame.ToUniversalTime().ToString("yyyy-MM-ddTHH:mm:ssZ")
+    $endDate = (Get-Date).ToUniversalTime().ToString("yyyy-MM-ddTHH:mm:ssZ")
 
-# Get current subscription name
-$subscriptionName = (Get-AzContext).Subscription.Name
+    # Get current subscription name
+    $subscriptionName = (Get-AzContext).Subscription.Name
 
-# Get SQL server details
-$sqlServers = Get-AzSqlServer
+    # Get SQL server details
+    $sqlServers = Get-AzSqlServer
 
-# Get metrics data for each SQL server
-foreach ($sqlServer in $sqlServers) {
+    # Get metrics data for each SQL server
+    foreach ($sqlServer in $sqlServers) {
 
-    $sqlServerName = $sqlServer.ServerName
-    $resourceGroupName = $sqlServer.ResourceGroupName
+        $sqlServerName = $sqlServer.ServerName
+        $resourceGroupName = $sqlServer.ResourceGroupName
 
-    # Get databases for the current SQL server
-    $databases = Get-AzSqlDatabase -ResourceGroupName $resourceGroupName -ServerName $sqlServerName
+        # Get databases for the current SQL server
+        $databases = Get-AzSqlDatabase -ResourceGroupName $resourceGroupName -ServerName $sqlServerName
 
-    foreach ($database in $databases) {
+        foreach ($database in $databases) {
 
-        $databaseName = $database.DatabaseName
-        $databaseResourceId = $database.ResourceId
-        $databaseSkuName = $database.SkuName
-        $CurrentServiceObjectiveName = $database.CurrentServiceObjectiveName
+            $databaseName = $database.DatabaseName
+            $databaseResourceId = $database.ResourceId
+            $databaseSkuName = $database.SkuName
+            $CurrentServiceObjectiveName = $database.CurrentServiceObjectiveName
 
-        # Define metrics parameters
-        $metricNames = @("allocated_data_storage", "storage", "cpu_percent", "sql_instance_memory_percent", "dtu_consumption_percent","dtu_limit","dtu_used")
-        $metricNamespace = "Microsoft.Sql/servers/databases"
+            # Define metrics parameters
+            $metricNames = @("allocated_data_storage", "storage", "cpu_percent", "sql_instance_memory_percent", "dtu_consumption_percent", "dtu_limit", "dtu_used")
+            $metricNamespace = "Microsoft.Sql/servers/databases"
 
-        # Initialize an object to store metrics data
-        $metricsData = [PSCustomObject]@{
-            'SubscriptionName'  = $subscriptionName
-            'SqlServerName'     = $sqlServerName
-            'DatabaseName'      = $databaseName
-            'ResourceGroupName' = $resourceGroupName
-            'databaseSkuName'   = $databaseSkuName
-            'CurrentServiceObjectiveName' = $CurrentServiceObjectiveName
-        }
+            # Initialize an object to store metrics data
+            $metricsData = [PSCustomObject]@{
+                'SubscriptionName'            = $subscriptionName
+                'SqlServerName'               = $sqlServerName
+                'DatabaseName'                = $databaseName
+                'ResourceGroupName'           = $resourceGroupName
+                'databaseSkuName'             = $databaseSkuName
+                'CurrentServiceObjectiveName' = $CurrentServiceObjectiveName
+            }
 
-        # Get metrics data for each metric
-        foreach ($metricName in $metricNames) {
-            $retryCount = 0
-            $maxRetries = 3
-            $retryDelay = 5
+            # Get metrics data for each metric
+            foreach ($metricName in $metricNames) {
+                $sqlMetric = Get-AzMetric -ResourceId $databaseResourceId -StartTime $startDate -EndTime $endDate -MetricNamespace $metricNamespace -MetricName $metricName -AggregationType Average -DetailedOutput -WarningAction SilentlyContinue
+                $sqlMetrics = $sqlMetric.Data
 
-            while ($retryCount -lt $maxRetries) {
-                try {
-                    $sqlMetric = Get-AzMetric -ResourceId $databaseResourceId -StartTime $startDate -EndTime $endDate -MetricNamespace $metricNamespace -MetricName $metricName -AggregationType Average -DetailedOutput -WarningAction SilentlyContinue
-                    $sqlMetrics = $sqlMetric.Data
+                # Calculate the total count for the current metric
+                $totalMetricCount = 0
+                $dataPointCount = 0
+                $nonZeroDataPointCount = 0
 
-                    # Calculate the total count for the current metric
-                    $totalMetricCount = 0
-                    $dataPointCount = 0
-                    $nonZeroDataPointCount = 0
+                foreach ($dataPoint in $sqlMetrics) {
+                    $value = $dataPoint.Average
 
-                    foreach ($dataPoint in $sqlMetrics) {
-                        $value = $dataPoint.Average
+                    # Exclude null or zero values
+                    if ($null -ne $value) {
+                        $totalMetricCount += $value
+                        $dataPointCount++
 
-                        # Exclude null or zero values
-                        if ($null -ne $value) {
-                            $totalMetricCount += $value
-                            $dataPointCount++
-
-                            if ($value -ne 0) {
-                                $nonZeroDataPointCount++
-                            }
+                        if ($value -ne 0) {
+                            $nonZeroDataPointCount++
                         }
                     }
-
-                    # Filter
-                    if ($database.CurrentServiceObjectiveName -like 'System*') {
-                        # Skip system databases
-                        continue
-                    }
-
-                    # Calculate the average if there are non-null, non-zero data points
-                    if ($dataPointCount -gt 0) {
-                        Write-Host "$metricName"
-                        $averageValue = $totalMetricCount / $dataPointCount
-                        
-                        # Additional formatting for specific metrics
-                        if ($metricName -eq "cpu_percent") {
-                            $averageValue = [math]::Round($averageValue, 5)
-                        }
-                        if ($metricName -eq "sql_instance_memory_percent") {
-                            $averageValue = [math]::Round($averageValue, 2)
-                        }
-                        if ($metricName -eq "storage" -or $metricName -eq "allocated_data_storage") {
-                            $averageValue = [math]::Round($averageValue / (1024 * 1024), 3)
-                        }
-                        if ($metricName -eq "dtu_used") {
-                            $averageValue = [math]::Round($averageValue, 3)
-                        }
-                        $metricsData | Add-Member -MemberType NoteProperty -Name $metricName -Value $averageValue -Force
-                    } else {
-                        $metricsData | Add-Member -MemberType NoteProperty -Name $metricName -Value 0 -Force
-                    }
-                } catch [System.Net.Http.HttpRequestException] {
-                    $retryCount++
-                    if ($retryCount -ge $maxRetries) {
-                        Write-Host "Error retrieving metric $metricName for database $metricsData.databaseName after $maxRetries attempts: $_"
-                        $metricsData | Add-Member -MemberType NoteProperty -Name $metricName -Value "Error" -Force
-                        break
-                    } else {
-                        Write-Host "Retrying metric $metricName for database $metricsData.databaseName ($retryCount/$maxRetries)..."
-                        Start-Sleep -Seconds $retryDelay
-                    }
-                } catch {
-                    Write-Host "Error retrieving metric $metricName for database $metricsData.databaseName: $_"
-                    $metricsData | Add-Member -MemberType NoteProperty -Name $metricName -Value "Error" -Force
-                    break
                 }
+
+                # Filter
+                if ($database.CurrentServiceObjectiveName -like 'System*') {
+                    # Skip system databases
+                    continue
+                }
+
+                # Calculate the average if there are non-null, non-zero data points
+                if ($dataPointCount -gt 0) {
+                    Write-Host "$metricName"
+                    $averageValue = $totalMetricCount / $dataPointCount
+                        
+                    # Additional formatting for specific metrics
+                    if ($metricName -eq "cpu_percent") {
+                        $averageValue = [math]::Round($averageValue, 5)
+                    }
+                    if ($metricName -eq "sql_instance_memory_percent") {
+                        $averageValue = [math]::Round($averageValue, 2)
+                    }
+                    if ($metricName -eq "storage" -or $metricName -eq "allocated_data_storage") {
+                        $averageValue = [math]::Round($averageValue / (1024 * 1024), 3)
+                    }
+                    if ($metricName -eq "dtu_used") {
+                        $averageValue = [math]::Round($averageValue, 3)
+                    }
+                    $metricsData | Add-Member -MemberType NoteProperty -Name $metricName -Value $averageValue -Force
+                }
+
+                # Add metrics data to results array
+                $resultsArray += $metricsData
             }
         }
-
-        # Add metrics data to results array
-        $resultsArray += $metricsData
     }
 }
-}
-
 # Save results to CSV file (overwrite if the file exists)
 $csvFilePath = "$outputpath\data.csv"
 $resultsArray | Select-Object -Property SubscriptionName, SqlServerName, DatabaseName, ResourceGroupName, databaseSkuName, CurrentServiceObjectiveName, 
-@{Name = "Data space allocated"; Expression = { $_.allocated_data_storage }}, 
-@{Name = "Data space used"; Expression = { $_.storage }}, 
-@{Name = "Percentage CPU"; Expression = { $_.cpu_percent }},
-@{Name = "DTU Limit"; Expression = { $_.dtu_limit }},
-@{Name = "DTU Used Average"; Expression = { $_.dtu_used }},
+@{Name = "Data space allocated"; Expression = { $_.allocated_data_storage } }, 
+@{Name = "Data space used"; Expression = { $_.storage } }, 
+@{Name = "Percentage CPU"; Expression = { $_.cpu_percent } },
+@{Name = "DTU Limit"; Expression = { $_.dtu_limit } },
+@{Name = "DTU Used Average"; Expression = { $_.dtu_used } },
 sql_instance_memory_percent | Export-Csv -Path $csvFilePath -NoTypeInformation -Force
 
 Write-Host "Results saved to $csvFilePath"
